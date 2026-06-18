facca43661f44ac38b4099aa65ab27653425a6e0
botwijns <siebgerben@gmail.com>
2026-06-18 17:15:31 +0200

    groups and carrying dynamic

--- players/human/human_player.gd
+++ players/human/human_player.gd
@@ -1,8 +1,8 @@
 class_name HumanPlayer extends Player
 
-signal interact
-var touching = null
-
+var touching: Node2D
+var carrying :Node2D
+signal pickup_objective(Entity: Node2D)
 
 func _ready() -> void:
 	$InteractionArea.area_entered.connect(_on_interaction_area_entered)
@@ -16,6 +16,11 @@ func _process(delta):
 			var groups = touching.get_groups()
 			print("interacting with interactable")
 			print(groups)
+			carrying = touching.duplicate()
+			carrying.scale = Vector2(0.5,0.5)
+			carrying.position = Vector2(0,-5)
+			add_child(carrying)
+			pickup_objective.emit(touching)
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -27,9 +32,10 @@ func _physics_process(delta):
 	
 func _on_interaction_area_entered(area: Area2D) -> void:
 	print("touch")
+	print(area.get_groups())
 	if area.is_in_group("interactable"):
 		print("interacts with other body ")
-		touching = area
+		touching = area.get_parent()
 		print("Player touched interactable!")
 		
 func _on_interaction_area_exited(body: Area2D) -> void:
