ffeeb5ddb73264ab7fe5a7f8b28d829e5cef7f30
botwijns <siebgerben@gmail.com>
2026-06-18 15:09:18 +0200

    added environment demo

---
+++ environment_demo/character_body_2d.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+signal interact
+var touching = null
+@export var speed = 200
+func _ready() -> void:
+	$InteractionArea.area_entered.connect(_on_interaction_area_entered)
+	$InteractionArea.area_entered.connect(_on_interaction_area_exited)
+	speed = 200
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		print("interact")
+		if(touching != null):
+			print("interacting with interactable")
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+func _on_interaction_area_entered(area: Area2D) -> void:
+	if area.is_in_group("interactable"):
+		print("interacts with other body ")
+		touching = area
+		print("Player touched interactable!")
+func _on_interaction_area_exited(body: Area2D) -> void:
+	touching = null
+	print("player stopped touching!")
+	#if body.is_in_group("interactable"):  # or check body is a Player class
+		#touching = null
+		#print("Player stopped touching!")
