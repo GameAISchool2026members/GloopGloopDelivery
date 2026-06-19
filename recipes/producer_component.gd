a8142d6d6140c99feb304ab56de92348e64609c4
botwijns <siebgerben@gmail.com>
2026-06-19 10:49:56 +0200

    furnace logic plus iron component

--- recipes/producer_component.gd
+++ recipes/producer_component.gd
@@ -1,3 +1,28 @@
 class_name ProducerComponent extends Node
 
 @export var recipes: Array[Recipe]
+@export var delay: int
+var production = false
+var output: Item
+func produce_item(input_recipe: Item) -> bool:
+	if production == false && output == null:
+		for recipe in recipes:
+			if recipe.input==input_recipe:
+				production = true
+				wait_on_production()
+				output = recipe.output
+				return true
+	return false
+
+func pickup() -> Item:
+	if production == true:
+		print("wanted to pickup item but component is busy")
+		return null
+	var placeholder = output
+	output = null
+	return placeholder
+
+func wait_on_production():
+	await get_tree().create_timer(delay).timeout
+	production = false
+	
