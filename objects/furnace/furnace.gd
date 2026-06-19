fbc22104016bb6d274a5737c9d9b40bb14af0a0d
bloepii <bloepii>
2026-06-19 11:56:47 +0200

    Furnace particles

--- objects/furnace/furnace.gd
+++ objects/furnace/furnace.gd
@@ -1,10 +1,12 @@
 extends Node2D
 @onready var producer_component = $ProducerComponent
 @onready var sprite = $Sprite2D
+@onready var particles: GPUParticles2D = $Particles
 var furnace_on = preload("res://objects/furnace/furnace_on.png")
 var furnace_off = preload("res://objects/furnace/furnace_off.png")
 
 func _process(delta):
+	particles.emitting = producer_component.production
 	if producer_component.production:
 		sprite.texture = furnace_on
 	else:
